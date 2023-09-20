57d88c278ae540defa4ebbfed5af1d648ceca101
Di Depan Layar <albinmaulana@yahoo.com>
2023-09-20 17:59:02 +0700

    Fix: Typo on Database Connection

--- classes/Database.php
+++ classes/Database.php
@@ -10,7 +10,7 @@ class Database {
     public function dbConnection() {
         try {
             $conn = new PDO('mysql:host=' . $this->db_host . ';dbname=' . $this->db_name, $this->db_username, $this->db_password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }
         catch(PDOException $e) {
